27b464e539afadbd2426877296e0b4d06290139f
tjdwns6872 <tjdwns6872@naver.com>
2024-06-12 20:29:13 +0900

    Input, Button 컴포넌트 생성

---
+++ community/src/features/authentication/components/form/Input.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Input = ({ children }) => {
+    return (
+        <input name={children}/>
+    );
+  };
+  
+  export default Input;
--- community/src/pages/Login.js
+++ community/src/pages/Login.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Input from '../features/authentication/components/form/Input';
+import Button from '../features/authentication/components/form/Button';
 
 const Login = () => {
   return (
-    <p>가장 먼저 보여지는 로그인페이지입니다.</p>
+    <div id="login_layout">
+      <p>가장 먼저 보여지는 로그인페이지입니다.</p>
+      <Input>userId</Input>
+      <Input>userPw</Input>
+      <Button>로그인</Button>
+    </div>
   );
 };
 
