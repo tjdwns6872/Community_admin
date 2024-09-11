7ce0969f1149720d7af8596e2ca1a140a86a1bf3
tjdwns6872 <tjdwns6872@naver.com>
2024-09-11 21:20:45 +0900

    .

--- community/src/layouts/Menu.js
+++ community/src/layouts/Menu.js
@@ -7,10 +7,7 @@ const Menu = () =>{
             <Link to={"/home"}>HOME</Link>
             {
                 sessionStorage.getItem('userData') !== null
-                ? <Button buttonId="btn login-btn" link='/' fname='logout'>LOGOUT</Button>
-                : <Link to={"/login"}>LOING</Link>
             }
-            <Button fname='test'>test</Button>
         </>
     )
 }
